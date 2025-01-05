a9aaf7c563e3218e43d885687b0bd06ae1f0304f
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-01-06 00:33:17 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# Запрос домена
-read -p "Введите домен: " DOMAIN
+read -p "Enter primary domain for Grafana, Prometheus and Node Exporter: " DOMAIN
 
-# Получение текущего IP-адреса сервера
 SERVER_IP=$(hostname -I | awk '{print $1}')
 
-# Создание конфигурации для Grafana
 cat <<EOF > /etc/nginx/conf.d/grafana.conf
 server {
     listen 8444 ssl;
@@ -25,7 +22,6 @@ server {
 }
 EOF
 
-# Создание конфигурации для Prometheus
 cat <<EOF > /etc/nginx/conf.d/prometheus.conf
 server {
     server_name prometheus.$DOMAIN;
@@ -46,7 +42,6 @@ server {
 }
 EOF
 
-# Создание конфигурации для Node Exporter
 cat <<EOF > /etc/nginx/conf.d/node-exporter.conf
 server {
     server_name node-exporter.$DOMAIN;
@@ -67,18 +62,11 @@ server {
 }
 EOF
 
-# Проверка и перезапуск Nginx
-echo "Проверяем конфигурацию Nginx..."
+echo "Checking Nginx configuration..."
 nginx -t && systemctl restart nginx
 
-# Установка Docker Compose
-echo "Устанавливаем Docker Compose..."
-apt install -y docker-compose
-
-# Создание папки для мониторинга
 mkdir -p /opt/monitoring/prometheus
 
-# Создание docker-compose.yml
 cat <<EOF > /opt/monitoring/docker-compose.yml
 services:
   grafana:
@@ -109,7 +97,6 @@ volumes:
     external: true
 EOF
 
-# Создание файла prometheus.yml
 cat <<EOF > /opt/monitoring/prometheus/prometheus.yml
 global:
   scrape_interval: 15s
@@ -137,18 +124,15 @@ scrape_configs:
       - targets: ['$SERVER_IP:9100']
 EOF
 
-# Создание volumes
 docker volume create grafana-storage
 docker volume create prom_data
 
-# Установка Node Exporter
 cd /opt/monitoring/
 wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.8.2.linux-amd64.tar.gz
 sudo cp node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin
 rm -rf node_exporter-1.8.2.linux-amd64 node_exporter-1.8.2.linux-amd64.tar.gz
 
-# Создание пользователя и сервиса Node Exporter
 sudo useradd --no-create-home --shell /bin/false node_exporter
 sudo chown node_exporter:node_exporter /usr/local/bin/node_exporter
 
@@ -170,20 +154,16 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-# Настройка UFW
 ufw allow from $SERVER_IP to any port 9100 proto tcp comment "Node Exporter"
 ufw allow from 172.17.0.0/16 to any port 9100 proto tcp comment "Node Exporter Docker Network 1"
 ufw allow from 172.18.0.0/16 to any port 9100 proto tcp comment "Node Exporter Docker Network 2"
 
-# Перезагрузка UFW для применения изменений
 ufw reload
 
-# Запуск Node Exporter
 sudo systemctl daemon-reload
 sudo systemctl enable node_exporter
 sudo systemctl start node_exporter
 
-# Запуск Docker Compose
 docker compose -f /opt/monitoring/docker-compose.yml up -d
 
 echo "Done."
