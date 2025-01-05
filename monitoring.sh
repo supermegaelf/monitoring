ed3f6c8743ef6262ad1892ce97bce9253c0384da
supermegaelf <81654098+supermegaelf@users.noreply.github.com>
2025-01-06 00:43:39 +0300

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -128,7 +128,7 @@ docker volume create grafana-storage
 docker volume create prom_data
 
 cd /opt/monitoring/
-wget https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
+wget -q https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
 tar xvf node_exporter-1.8.2.linux-amd64.tar.gz
 sudo cp node_exporter-1.8.2.linux-amd64/node_exporter /usr/local/bin
 rm -rf node_exporter-1.8.2.linux-amd64 node_exporter-1.8.2.linux-amd64.tar.gz
